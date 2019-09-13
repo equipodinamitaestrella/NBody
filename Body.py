412db5bff510ac3ad710d0b419b92f69ea7e65e7
CoolingBroski <camarenapliego@gmail.com>
2019-09-13 10:44:08 -0500

    Added __repr__ functionality

--- Body.py
+++ Body.py
@@ -5,8 +5,19 @@ class Body:
 		# pos and vel must be numpy arrays  
 		self.pos=pos
 		self.vel=vel
-		return self
+	
+	def __repr__(self):
+		return f'Body(pos:{self.pos}, vel:{self.vel})'
 	
 	def update_position(self, t_delta):
 		# t_delta is a scalar for transpired time
 		self.pos = self.pos + self.vel*t_delta
+
+
+#testing:
+'''
+body=Body(np.array((1,1,1)), np.array((1,1,1)))
+print(body)
+body.update_position(2)
+print(body)
+'''
