46a19ce11591b5d2bb3e26f7719d6295569b7f06
CoolingBroski <camarenapliego@gmail.com>
2019-10-10 15:10:53 -0500

    added more colors

--- plott.py
+++ plott.py
@@ -57,7 +57,7 @@ def dynamicPLot3d(system, particles):
     ax = fig.add_subplot(111, projection = '3d')
 
     i = 0
-    c = ['g', 'r', 'b']
+    c = ['g', 'r', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r']
 
     for particle in particles:
         time, trajectory = particle.getTrajectory()
